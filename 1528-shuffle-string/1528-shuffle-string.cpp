177f329358bc99d6bb0f895f474e7f16fea62b4f
Puneet Rai <73062696+Puneetpotter@users.noreply.github.com>
2022-10-02 00:24:49 +0530

    Time: 18 ms (40.13%), Space: 15.3 MB (42.89%) - LeetHub

---
+++ 1528-shuffle-string/1528-shuffle-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+        string ans;
+        ans.resize(s.size());
+        for(int i=0;i<s.size();i++)
+        {
+            ans[indices[i]]=s[i];
+        }
+        return ans;
+    }
+};
