0a57b53c28158a34bc578daefbcbee2ed475dddf
Puneet Rai <73062696+Puneetpotter@users.noreply.github.com>
2022-10-25 14:53:45 +0530

    Time: 8 ms (54.74%), Space: 6.8 MB (35.32%) - LeetHub

--- 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.cpp
+++ 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     bool areOccurrencesEqual(string s) {
        map<char,int>m;
-        for(int i=0;i<s.size();i++)
-            m[s[i]]=0;
+       // for(int i=0;i<s.size();i++)
+         //   m[s[i]]=0;
         for(int i=0;i<s.size();i++)
             m[s[i]]++;
         for(int i=0;i<s.size()-1;i++){
