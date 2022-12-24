b778bd6118bf2368675ee9569b42804640afa84b
Puneet Rai <73062696+Puneetpotter@users.noreply.github.com>
2022-12-24 13:01:06 +0530

    Time: 24 ms (90.61%), Space: 19.3 MB (15.91%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i =0;
+     //   if(nums.size()==1) return;
+        for(int j=0;j<nums.size();j++){
+            if(nums[j]!=0){
+                swap(nums[i],nums[j]);
+                i++;
+            }
+        }
+    }
+};
