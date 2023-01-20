ff9c0f80852150d00f39b81884a3100839dd17df
Puneet Rai <73062696+Puneetpotter@users.noreply.github.com>
2023-01-20 22:32:17 +0530

    Time: 4 ms (24.91%), Space: 6.3 MB (20.15%) - LeetHub

---
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    char repeatedCharacter(string s) {
+        
+        unordered_map <char, int> m;
+        
+        for(int i = 0; i < s.size(); i++){
+            
+            m[s[i]]++;
+            
+            if(m[s[i]] == 2)
+                return s[i];
+        }
+        
+        return ' ';
+    }
+};
