5a2971ea2651ba239fc63fc19cf9d0322b39c6c1
Puneet Rai <73062696+Puneetpotter@users.noreply.github.com>
2023-05-21 14:25:15 +0530

    Time: 1590 ms (15.30%), Space: 0B (100.00%) - LeetHub

---
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+SELECT s.user_id, ROUND(AVG(COALESCE(action = 'confirmed', 0)), 2) AS confirmation_rate FROM Signups s LEFT JOIN Confirmations c ON s.user_id = c.user_id GROUP BY user_id;
