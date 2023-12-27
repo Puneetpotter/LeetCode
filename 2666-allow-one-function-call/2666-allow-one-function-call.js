2b51f87c9d9260762d8df5a91ce0abf4e13cc1bc
Puneet Rai <73062696+Puneetpotter@users.noreply.github.com>
2023-12-27 13:20:35 +0530

    Time: 58 ms (17.58%), Space: 41.5 MB (89.94%) - LeetHub

---
+++ 2666-allow-one-function-call/2666-allow-one-function-call.js
@@ -0,0 +1,23 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    
+    let used = false;
+    return function(...args){
+        if(!used){
+            used = true;
+            return fn(...args);
+        }
+        return undefined;
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
