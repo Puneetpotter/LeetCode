5f16b7faac513f223e352f235f640f995b7a4177
Puneet Rai <73062696+Puneetpotter@users.noreply.github.com>
2024-01-12 19:24:55 +0530

    Time: 19 ms (95.21%), Space: 33.1 MB (29.40%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.cpp
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.cpp
@@ -11,38 +11,38 @@
  */
 class Solution {
 public:
-    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
-    if (root1 == nullptr && root2 == nullptr)
-        return nullptr;
-
-    if (root1 == nullptr)
-        return root2;
-
-    if (root2 == nullptr)
-        return root1;
-
-    TreeNode* root = new TreeNode(root1->val + root2->val);
-    root->left = mergeTrees(root1->left, root2->left);
-    root->right = mergeTrees(root1->right, root2->right);
-
-    return root;
-}
-};
 //     TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
-        
-//         if(root1 == NULL and root2 == NULL)
-//             return NULL;
-        
-//         if(root1 == NULL)
-//             return root2;
-        
-//         if(root2 == NULL)
-//             return root1;
-        
-//         TreeNode* root = new TreeNode(root1 -> val + root2 -> val);
-//         root -> left = mergeTrees(root1 -> left, root2 -> left);
-//         root -> right = mergeTrees(root2 -> right, root2 -> right);
-        
-//         return root;
-//     }
+//     if (root1 == nullptr && root2 == nullptr)
+//         return nullptr;
+
+//     if (root1 == nullptr)
+//         return root2;
+
+//     if (root2 == nullptr)
+//         return root1;
+
+//     TreeNode* root = new TreeNode(root1->val + root2->val);
+//     root->left = mergeTrees(root1->left, root2->left);
+//     root->right = mergeTrees(root1->right, root2->right);
+
+//     return root;
+// }
 // };
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+        
+        if(root1 == NULL and root2 == NULL)
+            return NULL;
+        
+        if(root1 == NULL)
+            return root2;
+        
+        if(root2 == NULL)
+            return root1;
+        
+        TreeNode* root = new TreeNode(root1 -> val + root2 -> val);
+        root -> left = mergeTrees(root1 -> left, root2 -> left);
+        root -> right = mergeTrees(root1 -> right, root2 -> right);
+        
+        return root;
+    }
+};
